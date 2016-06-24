45b988144355d21bb7ea764c74dc3ed942fa609f
pipipi <pi@raspberry>
2016-06-24 20:39:05 +0800

    add run script

--- setup_snapshot_with_ffmpeg.sh
+++ setup_snapshot_with_ffmpeg.sh
@@ -1,13 +1,26 @@
-cd ~
+#!/bin/bash
 
-# Compiling FFmpeg with
+cd ~
+git clone https://github.com/sunnyworm/snapshot_with_ffmpeg
+
+# note:
+# x264 commit 3f5ed56d4105f68c01b86f94f41bb9bbefa3433b
+# ffmpeg 18f687f73709a3ad5bb6b6fbbdbbce6dc8a91036
 # ./configure --arch=armel --target-os=linux --enable-gpl --enable-libx264 --enable-nonfree --extra-libs=-ldl --enable-libfreetype --enable-mmal
-wget http://163.22.32.59/ffmpeg.tar.gz
+
+wget http://cslab.im.ncnu.edu.tw/ffmpeg.tar.gz
+
 tar zxvf ffmpeg.tar.gz
 sudo mv ./ffmpeg /usr/local/bin/
 sudo mv ./ffprobe /usr/local/bin/
 sudo mv ./ffserver /usr/local/bin/
 rm ffmpeg.tar.gz
 
-sudo apt install ttf-wqy-microhei
+# for drawtext filter in ffmpeg
+sudo apt -y install ttf-wqy-microhei
 
+sudo cp /home/pi/snapshot_with_ffmpeg/snapshot.sh /opt/
+
+echo "Type sudo crontab -e and add entries like this:"
+echo "@reboot sleep 5; /usr/bin/screen -dmS snapshot bash -c 'sudo ~/snapshot_with_ffmpeg; exec bash'"
+echo "and this will run after rpi has booted"
---
+++ snapshot.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd /dev/shm &&
+/usr/local/bin/node /home/pi/snapshot_with_ffmpeg/snapshot.js
